095e0f6d72e754bff5f81041e05a288883c5e7d1
if0000 <if0000@users.noreply.github.com>
2023-10-20 07:41:31 +0200

    Plugin description update

--- divi-contact-form-file-attachment.php
+++ divi-contact-form-file-attachment.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Divi Contact Form File Attachment
-Plugin URI:  tech.ronizongor.com
+Plugin URI:  https://tech.ronizongor.com
 Description: Adds file attachment functionality to Divi Contact Form module
 Version:     1.0.0
 Author:      I. Finta @ Roni Zongor & Co / TECH
-Author URI:  tech.ronizongor.com
+Author URI:  https://tech.ronizongor.com
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain: dbrz-divi-contact-form-file-attachment
